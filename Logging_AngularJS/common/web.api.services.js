0b1d0e0d6e5267b30c3d7faaeedcac7aa86ba763
ZengetsuFR <jb.servicesweb@gmail.com>
2016-02-01 16:43:53 +0100

    update new url

--- Logging_AngularJS/common/web.api.services.js
+++ Logging_AngularJS/common/web.api.services.js
@@ -3,6 +3,6 @@
     angular
         .module("web.api.services", ["ngResource"])
         .constant("appSettings", {
-            serverPath: "https://microsoft-apiappd4677ce101bd4ae5988f568591e022c0.azurewebsites.net"
+            serverPath: "http://simpleloginwebapi.azurewebsites.net"
         });
 }());
